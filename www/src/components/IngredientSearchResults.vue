d70a00825c083cdeff9cab54126b044eac944367
Jeremiah Sprague <jeremiahsprague23@gmail.com>
2018-06-06 15:15:51 -0600

    searchByIngreidents done

--- www/src/components/IngredientSearchResults.vue
+++ www/src/components/IngredientSearchResults.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="IngredientSearchResults">
-Ingredienat Search IngredientSearchResults
+    <h1>Search by Ingredients in your Pantry!</h1>
+    <div>
+      <input type="text" v-model="query" placeholder="ex. burger, chicken, etc.">
+      <button @click="getSearchResults">Search All Recipes</button>
+      <div>
+        <ol>
+          <li v-for="recipe in recipes">
+            <h4>{{recipe.title}}</h4>
+            </li>
+        </ol>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,16 +20,29 @@ Ingredienat Search IngredientSearchResults
     name: 'IngredientSearchResults',
     data() {
       return {
+        recipe: {
+          title: '',
+          image: '',
+          spoonId: ''
+        },
+        query: ""
 
       }
     },
-    computed: {},
-    methods: {}
+    computed: {
+      //may want to change if multipe results are desired
+      recipes() {
+        return this.$store.state.recipes
+      }
+    },
+    methods: {
+      getSearchResults(){
+        this.$store.dispatch("getSearchIngredients",this.query)
+      }
+    }
   }
 
 </script>
 
 <style>
-
-
 </style>
