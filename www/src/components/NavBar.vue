f3eb44ec4adaa00499f83e204d9c472ed674f66a
Jeremiah Sprague <jeremiahsprague23@gmail.com>
2018-06-11 12:03:11 -0600

    FMCFF now navigates to home

--- www/src/components/NavBar.vue
+++ www/src/components/NavBar.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="NavBar">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">FoodyMcFoodface</a>
+        <router-link class="nav-link" :to="{name: 'Home'}">FoodyMcFoodface <span class="sr-only">(current)</span>
+        </router-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
         aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
