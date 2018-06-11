a5ccfb0d5ca9448802b4d30f3366c832c13b41bd
Jeremiah Sprague <jeremiahsprague23@gmail.com>
2018-06-11 11:07:31 -0600

    backgrounds added

--- www/src/components/profilePage.vue
+++ www/src/components/profilePage.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="ProfilePage">
-this is my profile page
-<favorites></favorites>
-  </div>
-</template>
+    <div class="container-fluid" id="background">
+      <div class="row">
+        <div class="col-3">
+        </div>
+        <div class="col-6">
+  
+          <favorites></favorites>
+        </div>
+        <div class="col-3"></div>
+      </div>
+    </div>
+  </template>
 
 <script>
   import Favorites from './Favorites'
@@ -24,6 +31,17 @@ this is my profile page
 </script>
 
 <style>
+  #background {
+    background-image: url("../assets/profilePage.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    /* background-size: contain; */
+    /* display: block; */
+    margin-left: auto;
+    margin-right: auto;
+    /* object-fit:cover; */
+  }
 
 
 </style>
