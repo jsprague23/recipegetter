e438840f9acd974bb0e9aacf6e3ba6c1d4363700
Jeremiah Sprague <jeremiahsprague23@gmail.com>
2018-06-16 23:40:36 -0600

    css

--- www/src/components/GeneralSearchResults.vue
+++ www/src/components/GeneralSearchResults.vue
@@ -10,7 +10,7 @@
           <!-- @click="setActiveRecipe(this.recipe)" -->
           <div class="card" v-for="recipe in recipes" :key="recipe.spoonId" >
             <router-link :to="{name: 'RecipeDetails', params:{spoonId: recipe.spoonId}}">
-              <img :src="'https://spoonacular.com/recipeImages/' + recipe.image">
+              <img class="card-img-top" :src="'https://spoonacular.com/recipeImages/' + recipe.image">
               <div class="card-body"> <h4 class="card-title">{{recipe.title}}</h4>
                 <h5 class="card-text">Cook Time: {{recipe.readyInMinutes}} minutes</h5></div>
               </router-link>
