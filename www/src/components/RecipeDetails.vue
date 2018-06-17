cef2ab7b1b68ebdd8b3abb4940021a5f76acda2f
Jeremiah Sprague <jeremiahsprague23@gmail.com>
2018-06-16 23:20:32 -0600

    css recipedetails

--- www/src/components/RecipeDetails.vue
+++ www/src/components/RecipeDetails.vue
@@ -1,57 +1,65 @@
 <template>
-    <div class="RecipeDetails container-fluid">
-        <div class="row">
-            <div class="col-12">
-                <h1>{{activeRecipe.title}}</h1>
-                <img :src="activeRecipe.image">
-            </div>
-            <div class="col-3"></div>
-            <div class="col-6 text-center">
-                {{activeRecipe.instructions}}
-
-                <div v-for="item in activeRecipe.ingredients">
-                    {{item.amount}} {{item.unit}} {{item.name}}
-                </div>
-                <button @click="addToFavorites">Favorite</button>
-                <div class="col-3"></div>
-            </div>
-        </div>
-
-
-
+  <div class="RecipeDetails container-fluid">
+    <div class="row">
+      <div class="col-3"></div>
+      <div class="col-6">
+        <h1>{{activeRecipe.title}}</h1>
+        <img :src="activeRecipe.image">
+      </div>
+      <div class="col-3"></div>
     </div>
+    <div class="row">
+        <div class="col-3"></div>
+    <div class="col-6 text-center">
+      {{activeRecipe.instructions}}
+
+      <div v-for="item in activeRecipe.ingredients">
+        {{item.amount}} {{item.unit}} {{item.name}}
+      </div>
+      <button @click="addToFavorites">Favorite</button>
+      <div class="col-3"></div>
+    </div>
+    </div>
+  </div>
+
+
+
+  </div>
 </template>
 
 
 <script>
-    import router from '../router'
-    export default {
-        name: 'RecipeDetails',
-        props: ['GeneralSearchResults'],
-        components: {},
-        mounted() {
-            // console.log(this.$route.params.spoonId)
-            this.$store.dispatch('getRecipeDetails', this.$route.params.spoonId)
-        },
-        data() {
-            return {
-                     
-            }
-        },
-        computed: {
-            activeRecipe() {
-                return this.$store.state.recipe
-            }
-        },
-        methods: {
-            addToFavorites(){
+  import router from '../router'
+  export default {
+    name: 'RecipeDetails',
+    props: ['GeneralSearchResults'],
+    components: {},
+    mounted() {
+      // console.log(this.$route.params.spoonId)
+      this.$store.dispatch('getRecipeDetails', this.$route.params.spoonId)
+    },
+    data() {
+      return {
+
+      }
+    },
+    computed: {
+      activeRecipe() {
+        return this.$store.state.recipe
+      }
+    },
+    methods: {
+      addToFavorites() {
         this.$store.dispatch('addToFavorites', this.activeRecipe)
       },
-        }
-
     }
+
+  }
+
 </script>
 
 
 <style scoped>
+
+
 </style>
