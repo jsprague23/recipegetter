7cfd58ae92bb2ced2159124e14c0a1fba5cb4b44
Edward Kearn <ejkearn@gmail.com>
2018-06-22 11:10:53 -0600

    cal routes started

--- server/routes/calItems.js
+++ server/routes/calItems.js
@@ -1,5 +1,54 @@
 var router = require('express').Router()
-var Recipe = require('../models/calItem')
+var CalItems = require('../models/calItem')
+
+//GET ALL
+router.get('/api/cal/:id', (req, res, next) => {
+  CalItems.find({userId: req.params.id})
+  .then(cal => {
+    res.send(cal)
+  })
+  .catch(err => {
+    res.status(400).send(err)
+  })
+})
+
+
+
+//EDIT
+  router.put('/api/cal/:id',   (req,res)=>{
+    CalItems.find({_id: req.params.id})
+    .then(function (cal) {
+      cal.push(req.body)
+      res.send(cal)
+    })
+    .catch(err=>{
+      res.status(400).send(err)
+    })
+  })
+
+
+// CREATE NEW
+router.post('/api/cal/', (req, res) => {
+  CalItems.create(req.body)
+  .then(newCal => {
+    res.send(newCal)
+  })
+  .catch(err => {
+    res.status(400).send(err)
+  })
+})
+
+//DELETE
+router.delete('/api/cal/:id', (req, res) => {
+  CalItems.findByIdAndRemove(req.params.id)
+  .then(() => {
+    res.send("Deleted!")
+  })
+  .catch(err => {
+    res.status(400).send(err)
+  })
+  
+})
 
 module.exports = {
   router
